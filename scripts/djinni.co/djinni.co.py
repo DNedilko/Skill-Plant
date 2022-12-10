5bad31a86712740cafd4517a74c4b01fce4fdfd8
illia <Illia.Streltsov@moyo.ua>
2022-12-10 14:58:18 -0500

    Added kafka to all loaders

--- scripts/djinni.co/djinni.co.py
+++ scripts/djinni.co/djinni.co.py
@@ -2,15 +2,15 @@ import requests
 import re
 import json
 import itertools
-import logging
-import time
+import sys
+sys.path.append('.')
 
+from Kafka.KafkaProducer import KafkaProducer
 from lxml import etree
 from bs4 import BeautifulSoup
 from multiprocessing import Pool, Lock
 from datetime import date
 from parser_raw import parse_date, parse_remote_type, parse_region, parse_seniority
-from confluent_kafka import Producer
 
 today = date.today()
 
@@ -22,6 +22,7 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 WORKERS = 20
 lock = Lock()
+kafka_producer = KafkaProducer()
 
 def write_to_json(vacancies):
     with open(file_name, 'w', encoding='utf-8') as f:
@@ -126,42 +127,10 @@ def get_jobs_data(job_link):
         'seniority': seniority,
         'date_gathered': today.strftime('%d/%m/%Y')
     }
-    produce_broker_message(vacancy)
+
+    kafka_producer.produce_broker_message(vacancy)
 
     return [vacancy]
-
-
-def set_logger():
-
-    logging.basicConfig(format='%(asctime)s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        filename='producer.log',
-                        filemode='w')
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    return logger
-
-
-def receipt(error, message):
-    logger = set_logger()
-    if error is not None:
-        print('Error: {}'.format(error))
-    else:
-        message_result = 'Produced message on topic {} with value of {}\n'.format(message.topic(), message.value().decode('utf-8'))
-        logger.info(message_result)
-        print(message_result)
-
-
-def produce_broker_message(data):
-
-    producer = Producer({'bootstrap.servers': 'localhost:9092'})
-    data_json = json.dumps(data)
-    producer.poll(1)
-    producer.produce('user-tracker', data_json.encode('utf-8'), callback=receipt)
-    producer.flush()
-    time.sleep(3)
-
 
 if __name__ == '__main__':
     pages = get_pages_links()
