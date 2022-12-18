371418c0cfeec16cf468bfd5387d666fb6c00210
DNedilko <dnedilko2002@gmail.com>
2022-12-18 19:39:36 +0100

    added description parser to djinni and changed abit database position load

--- scripts/description_parser.py
+++ scripts/description_parser.py
@@ -1,7 +1,20 @@
+<<<<<<< HEAD
 import itertools
 import json
 import spacy
 from deep_translator import GoogleTranslator
+=======
+import json
+
+import spacy
+from deep_translator import GoogleTranslator
+# load default skills data base
+from skillNer.general_params import SKILL_DB
+# import skill extractor
+from skillNer.skill_extractor_class import SkillExtractor
+from spacy.matcher import PhraseMatcher
+
+>>>>>>> eeae81e (added description parser to djinni and changed abit database position load)
 
 # loading description data from json
 def get_description(record):
@@ -15,6 +28,7 @@ def translation(initial_data):
     :param initial_data: initial description to parse
     :return: description translated to english
     '''
+<<<<<<< HEAD
     try:
         translator = GoogleTranslator(source='auto',target='en')
         english_text = translator.translate(initial_data)
@@ -25,10 +39,18 @@ def translation(initial_data):
 
 
 def skills_extraction(text, db):
+=======
+    translator = GoogleTranslator(source='auto',target='en')
+    english_text = translator.translate(initial_data)
+    return english_text
+
+def skills_extraction(text, nlp):
+>>>>>>> eeae81e (added description parser to djinni and changed abit database position load)
     '''
     :param text:
     :return:
     '''
+<<<<<<< HEAD
     empty1 = itertools.repeat(' ',5)
     empty2 = itertools.repeat(' ', 5)
     dict_skill = {"Hard Skill":[],"Soft Skill":[]} # ,"Soft Skill Ukr": [], "Hard Skill Ukr":[]}
@@ -60,10 +82,40 @@ def skills_extraction(text, db):
     #         dict_skill["Soft Skill Ukr"].append(skill.lower())
     #         appended+=1
     #         if appended>5: break
+=======
+    # init params of skill extractor
+    # spacy.cli.download("en_core_web_lg")
+
+    # init skill extractor
+    skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+    # extract skills from job_description
+    skills_dict = skill_extractor.annotate(text)
+    # print(skills_dict)
+    stop_s = 0
+    stop_h = 0
+    dict_skill = {"Hard Skill": [], "Soft Skill": []}
+    for k in skills_dict['results']:
+        skills = skills_dict['results'][k]
+        temp_skills_id = []
+        for el in skills:
+            if el['skill_id'] not in temp_skills_id:
+                temp_skills_id.append(el['skill_id'])
+
+                with open(".../skill_db_relax_20.json") as file:
+                    skills_db = json.load(file)
+                    type = skills_db[el['skill_id']]["skill_type"]
+                    if type == "Hard Skill" and stop_h<=5:
+                        dict_skill[type].append(el['doc_node_value'])
+                        stop_h+=1
+                    elif stop_s<=5 :
+                        dict_skill[type].append(el['doc_node_value'])
+                        stop_s+=1
+>>>>>>> eeae81e (added description parser to djinni and changed abit database position load)
 
 
     return dict_skill
 
+<<<<<<< HEAD
 def skills_extractor(job,skills_db):
 
 
@@ -73,10 +125,17 @@ def skills_extractor(job,skills_db):
 
     skills = skills_extraction(desc_translated, skills_db)
     print("Skills   ", skills)
+=======
+def skills_extractor(job, nlp):
+    desc = get_description(job)
+    desc_translated = translation(desc)
+    skills = skills_extraction(desc_translated, nlp)
+>>>>>>> eeae81e (added description parser to djinni and changed abit database position load)
     job.update(skills)
     # print(job)
 
 
+<<<<<<< HEAD
 if __name__ == "__main__":
 
     with open("C:/Users/dnedi/PycharmProjects/Skill-Plant/scripts/skills_data.json") as file:
@@ -86,3 +145,8 @@ if __name__ == "__main__":
     skills_db["Hard Skill Ukr"] = list(translation(text).split(";"))
 
     print("done")
+=======
+# if __name__ == "__main__":
+    # skills_extractor()
+
+>>>>>>> eeae81e (added description parser to djinni and changed abit database position load)
--- scripts/djinni.co/djinni.co.py
+++ scripts/djinni.co/djinni.co.py
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup
 from multiprocessing import Pool, Lock
 from datetime import date
 from parser_raw import parse_date, parse_remote_type, parse_region, parse_seniority
-from scripts.description_parser import skills_extractor
 
 today = date.today()
 
@@ -21,12 +20,10 @@ file_name = f"djinni.co_{today.strftime('%d-%m')}.json"
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
 
-WORKERS = 5
+WORKERS = 20
 lock = Lock()
 kafka_producer = KafkaProducer()
 
-with open("C:/Users/dnedi/PycharmProjects/Skill-Plant/scripts/skills_data.json") as file:
-    skills_db = json.load(file)
 
 def write_to_json(vacancies):
     with open(file_name, 'w', encoding='utf-8') as f:
@@ -44,7 +41,7 @@ def get_pages_links():
         print(f'Can`t get count of total pages.\n{e}\nPages set 600')
         total_pages = 600
 
-    pages_links = [page_next.format(page) for page in range(1, 50)]
+    pages_links = [page_next.format(page) for page in range(1, total_pages + 1)]
     return pages_links
 
 
@@ -132,21 +129,18 @@ def get_jobs_data(job_link):
         'date_gathered': today.strftime('%d/%m/%Y %H:%M:%S')
     }
 
-    vacancy_mod = skills_extractor(vacancy,skills_db)
-    # print(vacancy_mod)
     kafka_producer.produce_broker_message(vacancy)
 
     return [vacancy]
 
 
 if __name__ == '__main__':
-
     pages = get_pages_links()
 
     with Pool(WORKERS) as pool:
         jobs = list(itertools.chain(*pool.map(get_job_links, pages)))
 
     with Pool(WORKERS) as pool:
-        jobs_data = list(itertools.chain(*pool.map(get_jobs_data,jobs)))
+        jobs_data = list(itertools.chain(*pool.map(get_jobs_data, jobs)))
 
     # write_to_json(jobs_data)
