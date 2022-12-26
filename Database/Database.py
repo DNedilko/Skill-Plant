9df19a5abeaeddc4aa11d359786f214b0fe7fcd8
Daryna Nedilko <55350009+DNedilko@users.noreply.github.com>
2022-12-26 20:09:00 +0100

    Deleted unused

--- Database/Database.py
+++ Database/Database.py
@@ -9,14 +9,3 @@ class Database(metaclass=DatabaseSingleton):
 
     def getConnection(self):
         return self.__connection
-
-    def insert_row_into_database(self, data):
-
-        cursor = self.__connection.cursor()
-        columns = data.keys()
-        values = [data[column] for column in columns]
-        values_str_list = ["%s"] * len(values)
-        values_str = ", ".join(values_str_list)
-        cursor.execute("INSERT INTO song_table ({cols}) VALUES ({vals_str})".format(
-            cols=columns, vals_str=values_str), values)
-
